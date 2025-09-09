6d53f4bc95c595a303a79c9b8ef9a1ba3619249e
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2025-09-09 09:38:32 -0400

    setting up the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./App.css"
 
 const App = () => {
   return (
-    <div id="body" className="bg-blue-700 min-h-screen m-0 p-0">
-      <h1 className=" text-3xl w-1/2 bg-center">Assignment Tracker</h1>
+    <div id="container">
+      <h1 className="text-3xl text-white text-shadow-black text-shadow-md text-center p-2">Assignment Tracker</h1>
+      <p className="text-white text-xl text-shadow-black text-shadow-md text-center mr-2">Welcome to this assignment tracker! Click on the button to add an assignment and get started. Check out the Github repository for the project at the bottom of the website.</p>
+      <button type="button" className="rounded-full w-20 h-20 bg-white fixed bottom-3 right-3 font-semibold flex justify-center items-center"><p className="text-4xl">+</p></button>
     </div>
   )
 }
