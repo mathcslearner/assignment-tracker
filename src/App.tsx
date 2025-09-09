02003cfbb5ba178698a7b5186471feab17f5e9ad
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2025-09-08 23:20:00 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="bg-red-500 text-2xl">App</div>
+    <div id="body" className="bg-blue-700 min-h-screen m-0 p-0">
+      <h1 className=" text-3xl w-1/2 bg-center">Assignment Tracker</h1>
+    </div>
   )
 }
 
