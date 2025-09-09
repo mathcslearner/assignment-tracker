69e525a3d77a271ae605fffe34f7c693427f9dc7
mathcslearner <143976495+mathcslearner@users.noreply.github.com>
2025-09-09 16:44:32 -0400

    added form component and button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import "./App.css"
+import React, {useState} from 'react';
+import "./App.css";
+import AssignmentForm from './Modal.tsx';
+
 
 const App = () => {
+
+  //Determines whether the form is open or closed
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <div id="container">
       <h1 className="text-3xl text-white text-shadow-black text-shadow-md text-center p-2">Assignment Tracker</h1>
       <p className="text-white text-xl text-shadow-black text-shadow-md text-center mr-2">Welcome to this assignment tracker! Click on the button to add an assignment and get started. Check out the Github repository for the project at the bottom of the website.</p>
-      <button type="button" className="rounded-full w-20 h-20 bg-white fixed bottom-3 right-3 font-semibold flex justify-center items-center"><p className="text-4xl">+</p></button>
+      <button type="button" className="rounded-full w-20 h-20 bg-white fixed bottom-3 right-3 font-semibold flex justify-center items-center" onClick={() => {setIsOpen(!isOpen)}}><p className="text-4xl">+</p></button>
+      {isOpen && <AssignmentForm />}
     </div>
   )
 }
---
+++ src/Form.tsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+const AssignmentForm = () => {
+    return(
+        <div id="container">
+            <p>Hi</p>
+        </div>
+    )
+}
+
+export default AssignmentForm
